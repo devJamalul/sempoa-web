52455b4426c3bd837829e5812994a6f8a72c454b
AaEzha <aaezha@gmail.com>
2023-10-02 15:33:50 +0700

    chore: remove unused lines ❌

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -2,7 +2,6 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User';
 import Hash from '@ioc:Adonis/Core/Hash'
-// import phc from 'phc-bcrypt'
 
 export default class AuthController {
   public async registerShow({ view }: HttpContextContract) {
