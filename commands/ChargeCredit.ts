f78720df86657ea2930a0f235ba65267be4a2cd1
jamalul insan <jamal@marketport.io>
2023-10-25 16:31:49 +0700

    feat(recurring): recurring charge

--- commands/ChargeCredit.ts
+++ commands/ChargeCredit.ts
@@ -2,6 +2,10 @@ import { BaseCommand } from '@adonisjs/core/build/standalone'
 
 import { DateTime } from 'luxon'
 
+import Database from '@ioc:Adonis/Lucid/Database';
+
+import Logger from '@ioc:Adonis/Core/Logger';
+
 export default class ChargeCredit extends BaseCommand {
   /**
    * Command name is used to run the command
@@ -32,34 +36,88 @@ export default class ChargeCredit extends BaseCommand {
 
     const {default: Subscription } = await import('App/Models/Subscription');
     const {default: Company } = await import('App/Models/Company');
-    const {default: Payload } = await import('App/Models/Payload');
-    const now = DateTime.now();
-    const CompanyModel = await Company.findOrFail(8);
-    const test = await CompanyModel.related('subscriptions').create({
-      reference_number: 'Test',
-      package_name: 'test',
-      package_description: 'Test',
-      max_users: 2,
-      price: 3,
-      status: Subscription.STATUS_PENDING_PAYMENT,
-      start_date: now,
-      end_date: now,
-      is_recurring: true,
-      interval: 'MONTH',
-      interval_count: 2,
-      created_by:'tes',
-      updated_by: 'test',
-    })
-    test.status = Subscription.STATUS_ONGOING
-    test.save();
-    console.log(test.status)
-    // TODO: Find All Subscription Ongoing at expired today
-      // TODO: Charge Credit Total At fucntion
-    // subscription.forEach( async item => {
-     
-    // });
-    // // TODO: Send to xendit for charge
-    // TODO: Change Status sempoa if success
-    // this.logger.info(halo)
+    const {default: Payment } = await import('App/Models/Payment');
+    const {calculateMaxUsers, calculatePlan,} = await import('Helpers/calculatePlanHelper');
+    const {default: ChargeCreditCard } = await import('App/Services/ChargeCreditCard');
+    const {default: ErpIntegartion } = await import('App/Services/ErpIntegartion');
+
+
+    
+    const expiredToday:DateTime = DateTime.now();
+    const nextDay: number = 1;
+    const nextToStartDate:DateTime = DateTime.now().plus({day:nextDay});
+
+    const subscriptions = await Subscription.query()
+    .where('status',Subscription.STATUS_ONGOING)
+    .whereNot('package_name',Subscription.PACKAGE_TRIAL)
+    .where('end_date',expiredToday.toFormat('yyyy-LL-dd'))
+
+    subscriptions.map(async(item)=>{
+      const trx = await Database.transaction()
+      try {
+        // Start Depedency
+      const company = await Company.findOrFail(item.company_id);
+      const packageActive = item.package_name
+      const feePayByCustomer: number = calculatePlan(item.interval_count, packageActive, packageActive,nextDay)
+      const maxUser: number = calculateMaxUsers(packageActive)
+      item.status = Subscription.STATUS_TERMINATED
+      item.save()
+   
+
+      const subscription = await company.related('subscriptions').create({
+        reference_number: await Subscription.generateReferenceNumber(company.id),
+        package_name: item.package_name,
+        package_description: item.package_name + ' ' + item.interval_count + ' Bulan',
+        max_users: maxUser,
+        price: feePayByCustomer,
+        status: Subscription.STATUS_PENDING_PAYMENT,
+        start_date: nextToStartDate,
+        end_date: (item.interval_count > 1)
+          ? nextToStartDate.plus({ months: item.interval_count }).endOf('month')
+          : nextToStartDate.endOf('month'),
+        is_recurring: true,
+        interval: 'MONTH',
+        interval_count: item.interval_count,
+        created_by: 'sistem',
+        updated_by: 'sistem',
+      })
+
+
+      const payment = new Payment
+      payment.reference_number = await Payment.generateReferenceNumber()
+      payment.subscription_id = subscription.id
+      payment.amount = subscription.price
+      payment.status = Payment.STATUS_PENDING
+      payment.created_by = company.pic_name
+      payment.updated_by = company.pic_name
+      await payment.save()
+
+
+      // end depedency
+
+      const chargeCreditCard = new ChargeCreditCard();
+      chargeCreditCard.company = company;
+      chargeCreditCard.payment = payment;
+      chargeCreditCard.subcription = subscription;
+
+      const payToXendit = await chargeCreditCard.withXendit(feePayByCustomer)
+      if(payToXendit.status == 'failed') throw new Error(payToXendit.message)
+
+      subscription.status = Subscription.STATUS_ONGOING
+      subscription.save();
+
+      const erpIntegration = new ErpIntegartion()
+      erpIntegration.company = company;
+      erpIntegration.subscription = subscription;
+      erpIntegration.updateSubscription()
+
+      Logger.info(`Success For Recurring For Invoice : ${payment.reference_number}`)
+      await trx.transaction()
+      } catch (error) {
+        console.log(error)
+        Logger.warn(`Failed For Recurring: ${error.message}`)
+        await trx.rollback()
+      }
+    });
   }
 }
