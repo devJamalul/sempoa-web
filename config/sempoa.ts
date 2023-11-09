9222ae2ee0d8ddafe62c3e3c8debc9e32c3aea90
jamalul insan <jamal@marketport.io>
2023-11-09 15:50:43 +0700

    refs(config semmpoa): remove unused

--- config/sempoa.ts
+++ config/sempoa.ts
@@ -15,16 +15,4 @@ export default
       public_key: Env.get('PUBLIC_KEY_XENDIT'),
       webhook_verification_token: Env.get('WEBHOOK_VERIFICATION_TOKEN_XENDIT'),
     },
-
-    // xendit Pak Ivan
-    // xendit: {
-    //   secret_key: base64.encode('xnd_development_GKghcunUQ7bNkqohi7COW3FCoph29EXCOeiBY2XnFHe3qond2H999U9b5WMwn' + ':'),
-    //   public_key: 'xnd_public_development_gM6Y4Z3Chfq4zo86sq9bedaX61VVRAVf7rcxL2oUlCvHkwkswN74kQJoO6FX31',
-    // },
-
-    // xendit Pak Diar
-    // xendit: {
-    //   secret_key: base64.encode('xnd_development_kR2wasu27fdEpmIHs6tweGQaBUlMfWKLcEJkad7eHAfhCqhaSgwkYSpxtsF5' + ':'),
-    //   public_key: 'xnd_public_development_tlcpEgeix5OYJD7tkAFrVIl10APHMSumveFLBp2tSx7NLS3N4T1fep33QMIMS1J',
-    // },
   }
